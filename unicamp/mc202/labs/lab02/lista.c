7e4152fc64c9d3efb6cfdab2eca6c93871bb568e
Thiago Mendes <tribeirom@gmail.com>
2017-08-22 12:40:32 -0300

    [mc202:lab02]
    main separado da logica da lista

--- unicamp/mc202/labs/lab02/lista.c
+++ unicamp/mc202/labs/lab02/lista.c
@@ -4,38 +4,6 @@
 
 #include "lista.h"
 
-#define REQSIZE 7
-
-int32_t main(int argc, char **argv) {
-    list *lmtf = calloc(1, sizeof(list));
-    list *ltsp = calloc(1, sizeof(list));
-    list *lcnt = calloc(1, sizeof(list));
-    int32_t cmtf = 0, ctsp = 0, ccnt = 0;
-    int32_t req[REQSIZE] = {4, 2, 2, 4, 3, 1, 3};
-
-    for (int i = 5; i > 0 ; i--) {
-	inserir_lista(lmtf, i);
-	inserir_lista(ltsp, i);
-	inserir_lista(lcnt, i);
-    }
-
-    for (int i = 0; i < REQSIZE; i++) {
-	cmtf += req_move_to_front(lmtf, req[i]);
-	ctsp += req_transpose(ltsp, req[i]);
-	ccnt += req_count(lcnt, req[i]);
-    }
-
-    printf("Custo mtf: %d\n", cmtf);
-    printf("Custo tsp: %d\n", ctsp);
-    printf("Custo cnt: %d\n", ccnt);
-
-    imprimir_lista(lmtf, "lmtf");
-    imprimir_lista(ltsp, "ltsp");
-    imprimir_lista(lcnt, "lcnt");
-
-    return EXIT_SUCCESS;
-}
-
 void imprimir_cru(list *l) {
     if (l == NULL)
 	return;
