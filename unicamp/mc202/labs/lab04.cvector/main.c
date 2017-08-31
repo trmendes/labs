722d3f92757d1d702520a970477d6e95b9d216eb
Thiago Mendes <tribeirom@gmail.com>
2017-08-31 15:30:01 -0300

    [mc202:lab04:vector]
    scanf para ler a entrada como o analisador da unicamp espera

--- unicamp/mc202/labs/lab04.cvector/main.c
+++ unicamp/mc202/labs/lab04.cvector/main.c
@@ -4,33 +4,13 @@
 
 int main(int argc, char **argv) {
 
-    FILE *ifp;
-    //FILE *ofp;
-    int size, i;
-
-    if (argc < 2) {
-	printf("Eh necessario especificar o arquivo para o teste\n");
-	return EXIT_FAILURE;
-    }
-
-    ifp = fopen(argv[1], "r");
-    //ofp = fopen(argv[2], "w");
-
-    if (ifp == NULL) {
-	fprintf(stderr, "Nao consegui abrir o arquivo de entrada!\n");
-	exit(1);
-    }
-
-    //if (ofp == NULL) {
-	//fprintf(stderr, "Nao consegui abrir o arquivo de saida!\n");
-	//exit(1);
-    //}
+    int size = 1, i = 0;
 
     cvector_t *baralho = NULL;
     cvector_t *descartadas = NULL;
 
-    while (!feof(ifp)) {
-	fscanf(ifp, "%d", &size);
+    while (size > 0) {
+	scanf("%d", &size);
 	if (size == 0)
 	    break;
 
@@ -49,17 +29,12 @@ int main(int argc, char **argv) {
 	    cv_insert(baralho, cv_get(baralho));
 	}
 
-	//cv_write_output(ofp, descartadas, "Cartas jogadas:");
-	//cv_write_output(ofp, baralho, "Carta restante:");
 	cv_print(descartadas, "Cartas jogadas:");
 	cv_print(baralho, "Carta restante:");
     }
 
     cv_destroy(&baralho);
     cv_destroy(&descartadas);
-
-    fclose(ifp);
-    //fclose(ofp);
 
     return EXIT_SUCCESS;
 }
