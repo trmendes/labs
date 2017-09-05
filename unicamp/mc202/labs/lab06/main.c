0f76a3e177564c632b3431427fe18ced865a05d8
Thiago Mendes <tribeirom@gmail.com>
2017-09-05 20:08:31 -0300

    [mc202:lab06]
    main.c adaptado para o novo lab

--- unicamp/mc202/labs/lab06/main.c
+++ unicamp/mc202/labs/lab06/main.c
@@ -1,23 +1,45 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bits.h"
 
-#define ELEMENTS 100
+#define INPUTSIZE 100
 
 int32_t main(int32_t argc, char ** argv) {
-    uint8_t v[BIT_NBSLOTS(ELEMENTS)] = {0};
+    char cmd[INPUTSIZE];
+    int32_t argc1, argc2;
+    bits_t * bCtrl = NULL;
 
-    printf("Alocado um array de %d bytes\n", sizeof(v));
-    printf("Modificando o valor de um BIT na posicao %d com a mascara: 0x%x\n", 1, BIT_MASK(1));
-
-    BIT_SET(v,3);
-    PRINT_BSLOT(v,0);
-    BIT_SET(v,3);
-    if (BIT_TEST(v,3))
-	printf("Setado como 1\n");
-    else
-	printf("Setado como 0\n");
-
-    return EXIT_FAILURE;
+    while (1) {
+	scanf("%s", cmd);
+	if (strcmp(cmd, CMD_CREATE) == 0) {
+	    scanf("%d", &argc1);
+	    bCtrl = bt_create(argc1);
+	} else if (strcmp(cmd, CMD_ADD) == 0) {
+	    scanf("%d", &argc1);
+	    bt_add(bCtrl, argc1);
+	} else if (strcmp(cmd, CMD_REMOVE) == 0) {
+	    scanf("%d", &argc1);
+	    bt_remove(bCtrl, argc1);
+	} else if (strcmp(cmd, CMD_IN) == 0) {
+	    scanf("%d", &argc1);
+	    bt_is_in(bCtrl, argc1);
+	} else if (strcmp(cmd, CMD_RANK) == 0) {
+	    scanf("%d", &argc1);
+	    bt_rank(bCtrl, argc1);
+	} else if (strcmp(cmd, CMD_SELECT) == 0) {
+	    scanf("%d", &argc1);
+	    bt_select(bCtrl, argc1);
+	} else if (strcmp(cmd, CMD_RANGECNT) == 0) {
+	    scanf("%d %d", &argc1, &argc2);
+	    bt_rangecnt(bCtrl, argc1, argc2);
+	} else if (strcmp(cmd, CMD_PRINT) == 0) {
+	    bt_print(bCtrl);
+	} else if (strcmp(cmd, CMD_EXIT) == 0) {
+	    bt_exit(bCtrl);
+	    break;
+	}
+    }
+    return EXIT_SUCCESS;
 }
