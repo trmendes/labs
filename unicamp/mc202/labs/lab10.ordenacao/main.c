f3f2b06879a6faea1c864d55383524ecdb3fe56d
Thiago Mendes <tribeirom@gmail.com>
2017-10-30 15:51:25 -0200

    [mc202:lab:sort]
    Fix main

--- unicamp/mc202/labs/lab10.ordenacao/main.c
+++ unicamp/mc202/labs/lab10.ordenacao/main.c
@@ -7,8 +7,9 @@
 #include "main.h"
 #include "sort.h"
 
-#define ARRAY_SIZE 20
-#define SEED 2000
+#define REPEAT_FOR 10
+#define ARRAY_SIZE 60000
+#define MAX 2000
 
 int32_t main() {
     time_t t;
@@ -16,32 +17,36 @@ int32_t main() {
     int32_t array[ARRAY_SIZE];
     int32_t array_to_sort[ARRAY_SIZE];
 
-    size_t i;
+    size_t i, j;
 
-    for (i = 0; i < ARRAY_SIZE; ++i) {
-	array[i] = rand() % SEED;
+    for (j = 0; j < REPEAT_FOR; ++j) {
+	/* Media para X casos */
+	for (i = 0; i < ARRAY_SIZE; ++i) {
+	    array[i] = rand() % MAX; 
+	}
+
+	printf("----- LOOP #%ld\n", j);
+
+	memcpy(array_to_sort, array, sizeof(array));
+	sort_glibqsort((void *) array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
+	//    print(array_to_sort, ARRAY_SIZE);
+
+	memcpy(array_to_sort, array, sizeof(array));
+	sort_bubble(array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
+	//    print(array_to_sort, ARRAY_SIZE);
+
+	memcpy(array_to_sort, array, sizeof(array));
+	sort_selection(array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
+	//    print(array_to_sort, ARRAY_SIZE);
+
+	memcpy(array_to_sort, array, sizeof(array));
+	sort_insertion(array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
+	//    print(array_to_sort, ARRAY_SIZE);
+
+	memcpy(array_to_sort, array, sizeof(array));
+	sort_shell(array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
+	//    print(array_to_sort, ARRAY_SIZE);
     }
-
-    memcpy(array_to_sort, array, sizeof(array));
-    sort_glibqsort((void *) array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
-//    print(array_to_sort, ARRAY_SIZE);
-
-    memcpy(array_to_sort, array, sizeof(array));
-    sort_bubble(array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
-//    print(array_to_sort, ARRAY_SIZE);
-
-    memcpy(array_to_sort, array, sizeof(array));
-    sort_selection(array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
-//    print(array_to_sort, ARRAY_SIZE);
-
-    memcpy(array_to_sort, array, sizeof(array));
-    sort_insertion(array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
-//    print(array_to_sort, ARRAY_SIZE);
-
-    memcpy(array_to_sort, array, sizeof(array));
-    sort_shell(array_to_sort, ARRAY_SIZE, sizeof(int32_t), compare);
-//    print(array_to_sort, ARRAY_SIZE);
-
 
     return EXIT_SUCCESS;
 }
