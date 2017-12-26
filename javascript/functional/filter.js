cce9b106f60bc6a5aca4b3d30a2c62a89331f7f2
Thiago Mendes <thiago@posteo.de>
2017-12-26 20:13:46 -0200

    [javascript] filter more comments

--- javascript/functional/filter.js
+++ javascript/functional/filter.js
@@ -8,14 +8,21 @@ var village = [
     { name: "rafaela", species: "human" }
 ];
 
+/* In javascript function ARE VALUES
+ * and you can exploit it dividing your code into
+ * small simple function and COMPOSE them together
+ */
+function isItADog(val) {
+    return val.species === "dog";
+}
+
 /* filter will loop trough each item of the array
  * and for each item it will pass the callback
  * function which will return true or false.
  * In case of returning true filter will keep that value
  * In case of returning false filter won't keep that value
  */
-var dogs = village.filter(function(val) {
-    return val.species === "dog";
-});
+var dogs = village.filter(isItADog);
 
+console.log("Dogs:");
 console.log(dogs);
