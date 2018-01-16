f604ac743cd9fec1171271adf2765d37ec5696a3
Thiago Mendes <thiago@posteo.de>
2018-01-16 19:46:49 -0200

    [node]
    enconding the address from the terminal

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,8 +1,46 @@
-let request = require('request');
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+    a: {
+        demand: true,
+        alias: 'address',
+        describe: 'Address to fetch weather for',
+        /* to ALWAYS parse the arg as a string */
+        string: true
+    }
+})
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv);
+
+const encAddress = encodeURIComponent(argv.address);
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=av%20brasil',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encAddress}`,
+    /* We tell the server that we rather work with json than other format
+     * so the server can make our life easier and pack the response as a json
+     */
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    /* the body is the return of the http request
+     * could be a html, json, etc
+     * in this specific casa we are getting a json as a response to our
+     * request
+     */
+
+    /* response holds info about the request/server/response */
+
+    /* error holds info if something goes wrong like a server not found,
+     * internet is down, etc */
+
+    if (error == null) {
+        let lat = body.results[0].geometry.location.lat;
+        let lng = body.results[0].geometry.location.lng;
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Lat    : ${lat}`);
+        console.log(`Long   : ${lng}`);
+    }
 });
