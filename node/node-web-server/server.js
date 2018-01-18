d66fd2d138d7be023e15e32cbb26316a72d0d3cf
Thiago Mendes <thiago@posteo.de>
2018-01-18 21:44:16 -0200

    [node]
    Playing a bit with Handlebar
    
    a way to inject data into the website

--- node/node-web-server/server.js
+++ node/node-web-server/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const hbs = require('hbs');
 
 let app = express();
+
+
+app.set('view engine', 'hbs');
 
 /* Setup a server that publish the public directory */
 app.use(express.static(__dirname + '/public'));
@@ -10,14 +14,18 @@ app.use(express.static(__dirname + '/public'));
  * What do we send back to the person who made the request */
 app.get('/', (req, res) => {
     // res.send('<html><body><h1>Hello express</h1></body></html>');
-    res.send({
-        name: 'Andrew',
-        likes: ['biking', 'running']
+    res.render('home.hbs',{
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my webpage',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res) => {
