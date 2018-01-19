20fecf631a7a161f20688cfc0c15a78d620818a1
Thiago Mendes <thiago@posteo.de>
2018-01-19 15:40:57 -0200

    [node]
    Playing with partials and midleware

--- node/node-web-server/server.js
+++ node/node-web-server/server.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 
+/* Register a new middleware to log page call */
+app.use((req, res, next) => {
+    let now = new Date().toDateString();
+    /* Check the express response dot to see what we have access to */
+    let log = `${now}:  ${req.method} ${req.url}\n`; /* GET / */
+    fs.appendFile('server.log', log, error => {
+        if (error)
+            console.log("Unable to append to server.log");
+    });
+    next();
+});
+
+/* A middleware to `block` our website while we still working on it */
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
 /* Setup a server that publish the public directory */
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 /* Setup a handler (a router)
  *
@@ -16,15 +42,13 @@ app.get('/', (req, res) => {
     // res.send('<html><body><h1>Hello express</h1></body></html>');
     res.render('home.hbs',{
         pageTitle: 'Home Page',
-        welcomeMessage: 'Welcome to my webpage',
-        currentYear: new Date().getFullYear()
+        welcomeMessage: 'Welcome to my webpage'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
