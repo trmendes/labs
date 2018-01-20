b0e01953abf3e96ba5dfb65948683246e2b5c539
Thiago Mendes <thiago@posteo.de>
2018-01-20 18:22:42 -0200

    [node]
    Playing with Mocha, supertest and express

--- node/node-tests/server/server-test.js
+++ node/node-tests/server/server-test.js
@@ -1,13 +1,31 @@
 const request = require('supertest');
+const expect = require('expect');
 
 let app = require('./server').app;
 
 
-it ('should return hello world response', (done) => {
+it('should return response', (done) => {
     /* a supertest function */
     request(app)
-    .get('/')
-    .expect('Hello world')
-    .end(done); /* call done */
+        .get('/')
+        .expect(404)
+        .expect((res) => {
+            expect(res.body).toInclude({
+                error: 'Page not found'
+            });
+        })
+        .end(done); /* call done */
 });
 
+it('should return my user object', (done) => {
+    request(app)
+        .get('/users')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body).toInclude({
+                name: 'user 1',
+                age: 20
+            });
+        })
+        .end(done);
+});
--- node/node-tests/server/server.js
+++ node/node-tests/server/server.js
@@ -4,7 +4,30 @@ let app = express();
 
 
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.status(404).send({
+        error: 'Page not found',
+        name: 'Todo App v1.0'
+    });
+});
+
+//GET  /users - array of users
+
+app.get('/users', (req, res) => {
+    let users = [
+        {
+            name: "user 1",
+            age: 20
+        },
+        {
+            name: "user 2",
+            age: 30
+        },
+        {
+            name: "user 3",
+            age: 40
+        }];
+
+    res.status(200).send(users);
 });
 
 app.listen(3000);
