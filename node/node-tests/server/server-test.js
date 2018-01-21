e342ad172e46b1b2b4877b4f03acbd060aa5dfd1
Thiago Mendes <thiago@posteo.de>
2018-01-21 14:22:27 -0200

    [mocha]
    playing with describe

--- node/node-tests/server/server-test.js
+++ node/node-tests/server/server-test.js
@@ -4,28 +4,35 @@ const expect = require('expect');
 let app = require('./server').app;
 
 
-it('should return response', (done) => {
-    /* a supertest function */
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found'
-            });
-        })
-        .end(done); /* call done */
-});
+describe('Server tests', () =>  {
+    describe("GET /", () => {
+        it('should return response', (done) => {
+            /* a supertest function */
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found'
+                    });
+                })
+                .end(done); /* call done */
+        });
+    });
 
-it('should return my user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'user 1',
-                age: 20
-            });
-        })
-        .end(done);
+    describe("GET /users", () => {
+
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'user 1',
+                        age: 20
+                    });
+                })
+                .end(done);
+        });
+    });
 });
--- node/node-tests/utils/utils-test.js
+++ node/node-tests/utils/utils-test.js
@@ -2,69 +2,79 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-/* it is a function provided by mocha */
-it('should add two numbers', () => {
-    let res = utils.add(33,11);
-    // if (res !== 44)
-    //     throw new Error(`value not corret. Expected 44 but got ${res}`);
-    expect(res)
-        .toBe(44)
-        .toBeA('number');
 
-});
+describe('Utils tests' , () => {
+    describe('Simple functions', () => {
+        /* it is a function provided by mocha */
+        it('should add two numbers', () => {
+            let res = utils.add(33,11);
+            // if (res !== 44)
+            //     throw new Error(`value not corret. Expected 44 but got ${res}`);
+            expect(res)
+                .toBe(44)
+                .toBeA('number');
 
-it ('should square a number', () => {
-    let value = 3;
-    let res = utils.square(value);
-    // if (res !== 9)
-    //     throw new Error(`but I've got squara(${value}) = ${res} when I should get 9`);
-    expect(res)
-        .toBe(9)
-        .toBeA('number');;
-});
+        });
 
-it ('should excpect some values', () => {
-    // expect(12).toNotBe(11);
-    // Since they are not the same objects it will not work
-    // expect({name: 'Rodrigo'}).toBe({name: 'Rodrigo'});
-    expect([2,3,5]).toInclude(2);
-    expect([2,3,5]).toExclude(1);
-    expect({
-        name: 'Andrew',
-        age: 25,
-        location: 'Philadelphia'
-    })
-    .toInclude({
-        age: 25
+        it ('should square a number', () => {
+            let value = 3;
+            let res = utils.square(value);
+            // if (res !== 9)
+            //     throw new Error(`but I've got squara(${value}) = ${res} when I should get 9`);
+            expect(res)
+                .toBe(9)
+                .toBeA('number');;
+        });
     });
-});
 
-it ('should verify first and last names are set', () => {
-    let user = {
-        age: 25,
-        location: 'Philadelphia'
-    };
-    let res = utils.setName(user, "Rodrigo Rodolfo");
-    /* They should be equal because javascript passes objects as reference */
-    expect(user).toEqual(res);
-    expect(res).toInclude({
-        firstName: 'Rodrigo',
-        lastName: 'Rodolfo'
-    });
-});
+    describe("List/Json Tests", () => {
+        it ('should excpect some values', () => {
+            // expect(12).toNotBe(11);
+            // Since they are not the same objects it will not work
+            // expect({name: 'Rodrigo'}).toBe({name: 'Rodrigo'});
+            expect([2,3,5]).toInclude(2);
+            expect([2,3,5]).toExclude(1);
+            expect({
+                name: 'Andrew',
+                age: 25,
+                location: 'Philadelphia'
+            })
+                .toInclude({
+                    age: 25
+                });
+        });
 
-/* done tells mocha that this function is not done until done is called */
-it ('should wait for the asyncAdd', (done) => {
-    utils.asyncAdd(4,3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+        it ('should verify first and last names are set', () => {
+            let user = {
+                age: 25,
+                location: 'Philadelphia'
+            };
+            let res = utils.setName(user, "Rodrigo Rodolfo");
+            /* They should be equal because javascript passes objects as reference */
+            expect(user).toEqual(res);
+            expect(res).toInclude({
+                firstName: 'Rodrigo',
+                lastName: 'Rodolfo'
+            });
+        });
     });
-});
 
-/* Mocha will wait done call to decide wheather the test passed or not */
-it ('should wait for the asyncSquare', (done) => {
-    utils.asyncSquare(9, (square) => {
-        expect(square).toBe(81).toBeA('number');
-        done();
+    describe("Async functions teste", () => {
+        /* done tells mocha that this function is not done until done is called */
+        it ('should wait for the asyncAdd', (done) => {
+            utils.asyncAdd(4,3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            });
+        });
+
+        /* Mocha will wait done call to decide wheather the test passed or not */
+        it ('should wait for the asyncSquare', (done) => {
+            utils.asyncSquare(9, (square) => {
+                expect(square).toBe(81).toBeA('number');
+                done();
+            });
+        });
     });
+
 });
