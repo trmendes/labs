ab11ce94dc258f9df45818395d8bb093a1499656
Thiago Mendes <thiago@posteo.de>
2018-01-22 17:16:03 -0200

    [node mongodb]
    Playing with counter function

--- node/node-todo-api/playground/mongodb-find.js
+++ node/node-todo-api/playground/mongodb-find.js
@@ -9,6 +9,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     const db = client.db('TodoApp')
     console.log('Connected to MongoDB server');
 
+   db.collection('Todos').find().count().then((count) => {
+        console.log("# of Todos: ", count);
+    }, (err) => {
+        console.log("Unable to fetch todos: ", err);
+    });
+
     db.collection('Todos').find({
         _id: new ObjectID('5a6615b762c30b7ba14538d3')
     }).toArray().then((docs) => {
