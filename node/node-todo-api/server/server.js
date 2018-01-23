b83d93f5d45b6c35dd0bca1b400a61fd13e6e642
Thiago Mendes <thiago@posteo.de>
2018-01-23 18:03:50 -0200

    [node mongodb]
    Organizing files

--- node/node-todo-api/server/server.js
+++ node/node-todo-api/server/server.js
@@ -1,60 +1,26 @@
-'use strict'
+var express = require('express');
+var bodyParser = require('body-parser');
 
-const mongoose = require('mongoose');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-/* Will tell mongoose we want to use Promise */
-mongoose.Promise = global.Promise;
+var app = express();
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+app.use(bodyParser.json());
 
-/* Create a model for the Todo collection */
-let Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt:{
-        type: Number,
-        default: null
-    }
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
-let User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    }
-});
-
-/* Here we create a new document. Since none of the data is defined as
- * required we can go with the text property only for now
- */
-// let secTodo = new Todo({
-//     text: 'Learn how to cook',
-//     completed: true,
-//     completedAt: 123
-// });
-
-// secTodo.save().then((doc) => {
-//     console.log("Saved todo: ", doc);
-// }).catch((e) => {
-//     console.log("Unable to save todo");
-// });
-
-let user = new User({
-    email: "bla@bla.com"
-});
-
-user.save().then((doc) => {
-    console.log("Saved user:", doc);
-}).catch((e) => {
-    console.log("Unable to save user");
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
