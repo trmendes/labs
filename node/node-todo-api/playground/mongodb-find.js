f9667982933038bc6a84dacf25b2b6542aa51840
Thiago Mendes <thiago@posteo.de>
2018-01-23 13:57:11 -0200

    [mongodb node]
    fix error

--- node/node-todo-api/playground/mongodb-find.js
+++ node/node-todo-api/playground/mongodb-find.js
@@ -3,13 +3,13 @@
 const { MongoClient, ObjectID } = require('mongodb');
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
-    if (err) {
+    if (err)
         return console.log('Unable to connect to MongoDB server');
-    }
+
     const db = client.db('TodoApp')
     console.log('Connected to MongoDB server');
 
-   db.collection('Todos').find().count().then((count) => {
+    db.collection('Todos').find().count().then((count) => {
         console.log("# of Todos: ", count);
     }, (err) => {
         console.log("Unable to fetch todos: ", err);
