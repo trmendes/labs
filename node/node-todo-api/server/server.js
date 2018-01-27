ba8cf147bac88de4b9071558920fc02d56a831e8
Thiago Mendes <thiago@posteo.de>
2018-01-26 22:29:38 -0200

    [express mongoose]
    add delete route

--- node/node-todo-api/server/server.js
+++ node/node-todo-api/server/server.js
@@ -34,7 +34,7 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req , res) => {
-    var id = req.params.id;
+    let id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
@@ -48,6 +48,19 @@ app.get('/todos/:id', (req , res) => {
         res.send({todo});
     }).catch((e) => {
         res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo)
+            return res.status(404).send();
+        res.send({todo});
     });
 });
 
