de105ff7938810d912260bee2bdd379a88bee3c4
Thiago Mendes <thiago@posteo.de>
2018-01-27 14:25:41 -0200

    [node mocha]
    some new tests

--- node/node-todo-api/server/tests/server-test.js
+++ node/node-todo-api/server/tests/server-test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 123
 }];
 
 beforeEach((done) => {
@@ -135,4 +137,42 @@ describe('DEL /todos/:id', () => {
             end(done);
     });
 
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        let hexId = todos[0]._id.toHexString();
+        let text = "This should be the new text";
+        request(app).
+            patch(`/todos/${hexId}`).
+            send({
+                completed: true,
+                text
+            }).
+            expect(200).
+            expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                //expect(res.body.todo.completedAt).toBeA('number');
+            }).
+            end(done);
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        let hexId = todos[1]._id.toHexString();
+        let text = "This should be the new text!!!";
+        request(app).
+            patch(`/todos/${hexId}`).
+            send({
+                completed: false,
+                text
+            }).
+            expect(200).
+            expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toBe(null);
+            }).
+            end(done);
+});
 });
