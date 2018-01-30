55f7d8f502b92ce25fa6a1816e938982dde67a51
Thiago Mendes <thiago@posteo.de>
2018-01-30 18:25:05 -0200

    [node]
    test auth

--- node/node-todo-api/server/tests/server-test.js
+++ node/node-todo-api/server/tests/server-test.js
@@ -238,3 +238,41 @@ describe('POST /users', () => {
             end(done);
     });
 });
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app).
+            post('/users/login').
+            send(users[1]).
+            expect(200).
+            expect((res) => {
+                expect(res.headers['x-auth']).toBeTruthy();
+            }).
+            end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    // expect(user.tokens[0]).toInclude({
+                    //     access: 'auth',
+                    //     token: res.headers['x-auth']
+                    // });
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app).
+            post('/users/login').
+            send({
+                email: 'bla@bla.com',
+                password: '123'
+            }).
+            expect(400).
+            expect((res) => {
+                expect(res.headers['x-auth']).not.toBeTruthy();
+            }).
+            end(done);
+    });
+});
