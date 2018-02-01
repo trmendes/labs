000edcb59a2e7d2360680f3b507ede921eb762dd
Thiago Mendes <thiago@posteo.de>
2018-02-01 09:35:24 -0200

    [node expect]
    fix

--- node/node-todo-api/server/tests/server-test.js
+++ node/node-todo-api/server/tests/server-test.js
@@ -319,10 +319,10 @@ describe('POST /users/login', () => {
                     return done(err);
                 }
                 User.findById(users[1]._id).then((user) => {
-                    // expect(user.tokens[1]).toContain({
-                    //     access: 'auth',
-                    //     token: res.headers['x-auth']
-                    // });
+                    expect(user.toObject().tokens[1]).toMatchObject({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
                     done();
                 }).catch((e) => done(e));
             });
