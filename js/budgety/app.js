52e56140a3b5ab239dc52087be63ad3ab97d3adb
Thiago Mendes <thiago@posteo.de>
2018-02-04 20:35:46 -0200

    [js]
    playing a bit with closures and iffe
    
    private modules and public functions

--- js/budgety/app.js
+++ js/budgety/app.js
@@ -2,45 +2,65 @@
 
 // AN IIFE make everything inside it PRIVATE
 
+/* ----------------------------------------------------------------------------
+ * BUDGET CONTROLLER
+ * ----------------------------------------------------------------------------
+ */
 let budgetController = (
     () => {
-        /*
-         * Because of Clousers the publicTest function
-         * will ALWAYS have access to the x and add
-         */
-
-        /* Private */
-        let x = 23;
-
-        /* Private */
-        let add = (a) => {
-            return x + a;
-        }
-
-        /* Public */
-        return {
-            publicTest: (b) => {
-                return add(b);
-            }
-        }
+        return;
     }
 )();
 
+/* ----------------------------------------------------------------------------
+ * UI CONTROLLER
+ * ----------------------------------------------------------------------------
+ */
 let UIController = (
     () => {
 
+        let DOMStr = {
+            inputType: '.add__type',
+            inputDesc: '.add__description',
+            inputValue: '.add__value',
+            inputBtn: '.add__btn'
+        }
+
+        return {
+            getInput: () => {
+                return {
+                    // type === inc = + || exp = -
+                    type: document.querySelector(DOMStr.inputType).value,
+                    desc: document.querySelector(DOMStr.inputDesc).value,
+                    value: document.querySelector(DOMStr.inputValue).value
+                }
+            },
+            getDOMString: () => DOMStr
+        }
     }
 )();
 
+/* ----------------------------------------------------------------------------
+ * GLOBAL APP CONTROLLER
+ * ----------------------------------------------------------------------------
+ */
 let controller = (
     (budgetCtrl, UICtrl) => {
-        let z = budgetCtrl.publicTest(10);
-        return {
-            publicMethod: () => {
-                console.log(z);
+
+        let DOMStr = UICtrl.getDOMString();
+
+        let ctrlAddItem = () => {
+            let input = UICtrl.getInput();
+            console.log(input);
+        };
+
+        document.querySelector(DOMStr.inputBtn).addEventListener('click',
+            ctrlAddItem);
+        document.addEventListener('keypress', (event) => {
+            if (event.charCode === 0) {
+                ctrlAddItem();
             }
-        }
+        });
     }
 )(budgetController, UIController);
 
-controller.publicMethod();
