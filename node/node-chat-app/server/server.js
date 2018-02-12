75476e65c4fa6216a19f438e37ca7f4edad2efb6
Thiago Mendes <thiago@posteo.de>
2018-02-12 09:38:56 -0200

    node: socket.io
    Create a connection in btw the browser and the server

--- node/node-chat-app/server/server.js
+++ node/node-chat-app/server/server.js
@@ -2,15 +2,43 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 
-const socketIO = require('socket.io');
-const publicPath = path.join(__dirname, '..', 'public');
 const app = express();
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-const io = socketIO(server);
 
+/* Creating the public directory path */
+const publicPath = path.join(__dirname, '..', 'public');
+
+const socketIO = require('socket.io');
+const server = http.createServer(app);
+
+/* A middleware to take all requests to the public
+ * directory
+ */
 app.use(express.static(publicPath));
 
+/* We want to create an IO socket using
+ * our http server
+ */
+/* This socketIO lib will give us a
+ * script to use at the browser
+ * we have access to it using the follow addr
+ * localhost:port/socket.io/socket.io.js
+ */
+const io = socketIO(server);
+
+io.on('connection', function (socket) {
+    console.log('a new connection from a client');
+    socket.on('disconnect', () => {
+        console.log('The same client went away');
+    });
+});
+
+
+const port = process.env.PORT || 3000;
+
+
+/* Not using the app to listen
+ * using the http server
+ */
 server.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
