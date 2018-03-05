268c2d8ac1c2e4dc1e9aaaad727f57dcd33a8b2a
Thiago Mendes <thiago@posteo.de>
2018-03-05 16:27:49 -0300

    react: just a bit more from react

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -4,6 +4,13 @@ console.log('app.js  is running');
 
 const appRoot = document.getElementById('app');
 
-let template = <div><h1>This is JSX from app.js!...</h1><p>Cool!</p></div>;
+const template = <div><h1>This is JSX from app.js!...</h1><p>Cool!</p></div>;
 
-ReactDOM.render(template, appRoot);
+const template2 = (
+    <div>
+        <h1>Zeh Silva</h1>
+        <p>Age: 345</p>
+        <p>Location: Nepal</p>
+    </div>);
+
+ReactDOM.render(template2, appRoot);
