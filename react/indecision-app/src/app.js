3238441b7539f8b988ad168d5491eb32a0dab609
Thiago Mendes <thiago@posteo.de>
2018-03-05 16:16:20 -0300

    react: We must have a single root element and wrap all the other elements nested

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -4,6 +4,6 @@ console.log('app.js  is running');
 
 const appRoot = document.getElementById('app');
 
-let template = <h1>This is JSX from app.js!...</h1>;
+let template = <div><h1>This is JSX from app.js!...</h1><p>Cool!</p></div>;
 
 ReactDOM.render(template, appRoot);
