ec98e358f36bcfb90e35037972282c70caab4368
Thiago Mendes <thiago@posteo.de>
2018-03-05 17:28:01 -0300

    react: playing a bit with the jsx syntax

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -12,7 +12,12 @@ const app = {
 const template = (
     <div>
         <h1>{app.title.toUpperCase()}</h1>
-        <p>{app.subtitle}</p>
+        {app.subtitle && <p>{app.subtitle}</p>}
     </div>);
+
+// {app.subtitle && <p>{app.subtitle}</p>}
+/* if app.subtitle exists show <p>app.stubtitle</p>
+ * else don't show anything
+ */
 
 ReactDOM.render(template, appRoot);
