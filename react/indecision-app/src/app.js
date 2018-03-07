0977ba1c209dd24d1fd16d040a06c3f8b51dc39f
Thiago Mendes <thiago@posteo.de>
2018-03-07 20:04:35 -0300

    react: playing with React components

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -1,57 +1,52 @@
-'use strict';
-
-console.log('app.js  is running');
-
-const appRoot = document.getElementById('app');
-
-const app = {
-    title: 'Web page',
-    subtitle: 'This is our first example using JSX',
-    options: []
-};
-
-const onFormSubmit = (e) => {
-    /* Stop the full page refresh */
-    e.preventDefault();
-    const option = e.target.elements.opicao.value;
-    if (option) {
-        app.options.push(option);
-        e.target.opicao.value = '';
-        console.log(app.options);
-        renderApp();
+class Header extends React.Component {
+     /* you must define render */
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        );
     }
 };
 
-const removeAll = () => {
-    app.options = [];
-    renderApp();
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What Should I do?</button>
+            </div>
+        );
+    }
 };
 
-let updateArray = () => {
-    let key = 0;
-        console.log(jsXOptions);
-    return jsXOptions;
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Option component here!</p>
+            </div>
+        );
+    }
 };
 
-const renderApp = () => {
-    const template = (
+class AddOptions extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>AddOptions Component here!</p>
+            </div>
+        );
+    }
+};
+
+const jsx = (
     <div>
-        <h1>{app.title.toUpperCase()}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
-        <p>Options counter: {app.options.length}</p>
-        <ol>
-            {
-                app.options.map(el => <li key={el}>{el}</li>)
-            }
-        </ol>
-        <form onSubmit={onFormSubmit}>
-            <input type="text" name='opicao' placeholder="describe a new option"></input>
-            <button>Add Option</button>
-        </form>
-        <button onClick={removeAll}>Remove All</button>
-    </div>);
-    ReactDOM.render(template, appRoot);
-};
+        <Header />
+        <Action />
+        <Options />
+        <AddOptions />
+    </div>
+);
 
-renderApp();
+ReactDOM.render(jsx, document.getElementById('app'));
