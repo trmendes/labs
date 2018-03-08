a7927b54005c857971d9e995a76b090a59facd81
Thiago Mendes <thiago@posteo.de>
2018-03-08 12:54:41 -0300

    react: default prop values

--- react/indecision-app/src/app.js
+++ react/indecision-app/src/app.js
@@ -6,7 +6,7 @@ class IndecisionApp extends React.Component {
         this.handleAddOption = this.handleAddOption.bind(this);
 
         this.state = {
-            options: []
+            options: props.options
         };
     }
 
@@ -47,7 +47,7 @@ class IndecisionApp extends React.Component {
 
         return (
             <div>
-                <Header title={title} subtitle={subtitle}/>
+                <Header subtitle={subtitle}/>
                 <Action
                     hasOptions={this.state.options.length > 0}
                     pick={this.handlePick}
@@ -66,13 +66,21 @@ class IndecisionApp extends React.Component {
     };
 };
 
+IndecisionApp.defaultProps = {
+    options: []
+};
+
 const Header = (props) => {
     return (
         <div>
             <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
+            {props.subtitle && <h2>{props.subtitle}</h2>}
         </div>
     );
+};
+
+Header.defaultProps = {
+    title: 'Indecision'
 };
 
 const Action = (props) => {
