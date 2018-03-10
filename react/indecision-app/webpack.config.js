95452d5c559ec37de763a0ce85fc1cc5095691dd
Thiago Mendes <thiago@posteo.de>
2018-03-10 09:31:16 -0300

    webpack: configure it

---
+++ react/indecision-app/webpack.config.js
@@ -0,0 +1,9 @@
+const path = request('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+};
