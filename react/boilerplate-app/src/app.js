b839f3077d07c973300cc7c0f49a73d985eb41bf
Thiago Mendes <thiago@posteo.de>
2018-03-14 11:19:37 -0300

    react routing: create client side routes

--- react/boilerplate-app/src/app.js
+++ react/boilerplate-app/src/app.js
@@ -1,7 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const expenseDashboardPage = () => (
+    <div>
+        This is from my Dashboard component
+    </div>
+);
+
+const addExpensePage = () => (
+    <div>
+        This is from my addExpense component
+    </div>
+);
+
+const editExpensePage = () => (
+    <div>
+        This is from my editExpense component
+    </div>
+);
+
+const helpExpensePage = () => (
+    <div>
+        This is from my helpExpense component
+    </div>
+);
+
+const notFoundPage = () => (
+    <div>
+        Page not found
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+        <Switch>
+            <Route
+                path="/"
+                component={expenseDashboardPage}
+                exact={true}
+            />
+            <Route
+                path="/create"
+                component={addExpensePage}
+            />
+            <Route
+                path="/edit"
+                component={editExpensePage}
+            />
+            <Route
+                path="/help"
+                component={helpExpensePage}
+            />
+            <Route
+                component={notFoundPage}
+
+            />
+        </Switch>
+    </BrowserRouter>
+);
+ReactDOM.render(routes , document.getElementById('app'));
