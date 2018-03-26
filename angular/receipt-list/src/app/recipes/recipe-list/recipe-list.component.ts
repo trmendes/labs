1aaa342436d37f984e9e30cba60d12d6c8ea40a4
Thiago Mendes <thiago@posteo.de>
2018-03-26 11:19:52 -0300

    angular: detail template

--- angular/receipt-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/receipt-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is just a test',
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Pamonhas.jpg/500px-Pamonhas.jpg'),
+		new Recipe('Test Recipe 2', 'This is just a second test',
+			'https://3.bp.blogspot.com/-IkXXpkCnu8c/Uaqsw2DuFNI/AAAAAAAAAS8/QwIfMdxXWYc/s1600/receita-curau-eduardo-delfim.jpg')
+  ];
 
   constructor() { }
 
--- angular/receipt-list/src/app/shopping-list/shopping-list.component.ts
+++ angular/receipt-list/src/app/shopping-list/shopping-list.component.ts
@@ -1,18 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from '../recipes/recipe.model';
-
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is just a test',
-      'https://en.wikipedia.org/wiki/Pamonha#/media/File:Pamonhas.jpg')
-  ];
-
   constructor() { }
 
   ngOnInit() {
