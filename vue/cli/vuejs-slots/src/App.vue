3bf5a8f0c484ed17d11f6017643365f05c54503e
Thiago Mendes <thiago@posteo.de>
2018-05-22 18:38:51 -0300

    vue: playing with slots (they are pretty cool)...

---
+++ vue/cli/vuejs-slots/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <form-helper>
+      <div slot="slot-form-header">
+        <h3>This is the title of the form</h3>
+        <p>information about the form</p>
+      </div>
+      <div slot="slot-form-fields">
+        <input type="text" placeholder="name" required>
+        <input type="password" placeholder="password" required>
+      </div>
+      <div slot="slot-form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+
+import formHelper from './components/formHelper.vue'
+
+export default {
+  components: {
+    'form-helper': formHelper
+  },
+  data () {
+    return {
+      title: 'ScopedTitle'
+    };
+  },
+  methods: {
+    handleSubmit: function() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue/cli/vuejs-slots/src/components/formHelper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Please fill out our form</h1>
+    <form>
+      <div id="form-header">
+        <slot name="slot-form-header"></slot>
+      </div>
+      <div id="form-fields">
+        <slot name="slot-form-fields"></slot>
+      </div>
+      <div id="form-controls">
+        <slot name="slot-form-controls"></slot>
+      </div>
+      <div id="useful-links">
+        <ul>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+          <li><a href="#">Link 4</a></li>
+        </ul>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  componets: {
+
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</stule>
