2a21981945e217ea820b1d19cc37cbb81edee279
Thiago Mendes <thiago@posteo.de>
2018-05-22 11:18:47 -0300

    vue: playing with the vue-cli

---
+++ vue/cli/vuejs-playlist/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>{{ title }} </h1>
+    <p> {{ greeting() }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'Your first Vue file, woooo!'
+    }
+  },
+  methods: {
+    greeting: function() {
+      return 'hi there';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
