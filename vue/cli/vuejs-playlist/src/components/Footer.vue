0355be0bfe75e79676869d0764c15238bd80d3d8
Thiago Mendes <thiago@posteo.de>
2018-05-22 14:33:46 -0300

    vue: playing with components and style

---
+++ vue/cli/vuejs-playlist/src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <p>{{ copyright }}</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      copyright: 'Creative Commons'
+    }
+  }
+}
+</script>
+
+<style scoped>
+footer {
+  background: #222;
+  padding: 6px;
+}
+
+p {
+  color: lightgreen;
+  text-align: center;
+}
+
+</style>
