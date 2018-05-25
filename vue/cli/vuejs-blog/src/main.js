0ffde5841852c2892fa7a0b86271c8e3a73c5e39
Thiago Mendes <thiago@posteo.de>
2018-05-25 16:51:14 -0300

    vue: custom directives :O :O :O. Amazing :)

--- vue/cli/vuejs-blog/src/main.js
+++ vue/cli/vuejs-blog/src/main.js
@@ -2,7 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
 
-Vue.use(VueResource)
+Vue.use(VueResource);
+
+/* Custom directive */
+Vue.directive('raibow', {
+  bind(el, binding, virtualNode) {
+    el.style.color = "#" + Math.random().toString().slice(2,8);
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding, virtualNode) {
+    if (binding.value === "wide") {
+      el.style.maxWidth = "1200px";
+    } else if (binding === "narrow") {
+      el.style.maxWidth = "320px";
+    }
+
+    if (binding.arg === "column") {
+      el.style.background = "#ddd";
+      el.style.padding = "20px";
+    }
+  }
+
+});
 
 new Vue({
   el: '#app',
