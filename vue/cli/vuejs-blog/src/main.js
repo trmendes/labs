ff502b8c7d81738fe43260096c20323875677d03
Thiago Mendes <thiago@posteo.de>
2018-05-26 16:24:06 -0300

    vue: learning about filters

--- vue/cli/vuejs-blog/src/main.js
+++ vue/cli/vuejs-blog/src/main.js
@@ -24,7 +24,16 @@ Vue.directive('theme', {
       el.style.padding = "20px";
     }
   }
+});
 
+/* Our filters */
+
+Vue.filter('filter-to-uppercase', function(value) {
+  return value.toUpperCase();
+});
+
+Vue.filter('snippet', function(value) {
+  return value.slice(0,100) + '...';
 });
 
 new Vue({
