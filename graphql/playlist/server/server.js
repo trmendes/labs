cad35cdac9b9122d2d5ac59db39d01de3eb95cd9
Thiago Mendes <thiago@posteo.de>
2018-06-20 23:31:48 -0300

    graphQL: Lists

--- graphql/playlist/server/server.js
+++ graphql/playlist/server/server.js
@@ -2,10 +2,18 @@
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/graphql-playlist').then(() => {
+    console.log('connected to mongodb');
+}).catch(e => {
+    console.log('mongodb is not running');
+    process.exit(1);
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
