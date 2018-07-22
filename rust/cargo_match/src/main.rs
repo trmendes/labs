07206bda090420782fb68986c4401ce5f87ba640
Thiago Mendes <thiago@posteo.de>
2018-07-22 12:57:55 -0300

    rust: match

---
+++ rust/cargo_match/src/main.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let value_in_cents = value_in_cents(Coin::Quarter(UsState::Alaska));
+    println!("Value in cents: {}", value_in_cents);
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        },
+    }
+}
